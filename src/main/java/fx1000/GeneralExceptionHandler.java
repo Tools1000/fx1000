128d0458810d9e92548e8ea285e2dd560e22edc9
Alexander Kerner <alexkerner1000@gmail.com>
2023-03-10 12:08:09 +0100

    Fix imports

--- src/main/java/fx1000/GeneralExceptionHandler.java
+++ src/main/java/fx1000/GeneralExceptionHandler.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import tools1000.ToolsUtil;
 
 import java.util.ResourceBundle;
 
